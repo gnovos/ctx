5e5896f50aa4a31c34eab71c12e1e613760e096d
Mason <freemason@gmail.com>
2012-10-29 23:34:45 -0700

    cleanup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source "http://rubygems.org"
 
 gem 'rake'
-gem 'sender'
 
 gemspec
 
--- spec/specs/ctx_spec.rb
+++ spec/specs/ctx_spec.rb
@@ -1,65 +1,66 @@
-require '../spec_helper'
+require 'spec_helper'
+require 'ctx'
 
 describe CTX do
 
-  it "Can scope method definitions within a dynamic scope" do
+  it "Can scope method definitions within a dynamic context" do
 
-    class ScopeTest
-      def scoped_method(num)
+    class ContextTest
+      def some_method(num)
         "original context : #{num}"
       end
     end
 
-    class ScopeTest
-      ctx_define :bar, :scoped_method do |num|
+    class ContextTest
+      ctx_define :bar, :some_method do |num|
         "bar context : #{num * 2}"
       end
 
-      ctx_define :foo, :scoped_method do |num|
+      ctx_define :foo, :some_method do |num|
         "foo context : #{num / 2}"
       end
 
-      ctx_define :scoped_method do |num|
+      ctx_define :some_method do |num|
         "anonymous context : #{num + 2}"
       end
 
     end
 
-    scoped = ScopeTest.new
+    context = ContextTest.new
 
     ctx.should be_nil
 
-    scoped.scoped_method(10).should == "original context : 10"
+    context.some_method(10).should == "original context : 10"
 
     ctx :foo do
       ctx.name.should == :foo
       ctx[:var] = "in foo context"
       ctx[:var].should == "in foo context"
 
-      scoped.scoped_method(10).should == "foo context : 5"
+      context.some_method(10).should == "foo context : 5"
 
       ctx(:bar) do
         ctx.name.should == :bar
         ctx[:var] = "in bar context"
         ctx[:var].should == "in bar context"
 
-        scoped.scoped_method(10).should == "bar context : 20"
+        context.some_method(10).should == "bar context : 20"
         ctx :unknown do
-          scoped.scoped_method(10).should == "bar context : 20"
+          context.some_method(10).should == "bar context : 20"
         end
       end
 
       ctx.name.should == :foo
 
-      scoped.scoped_method(10).should == "foo context : 5"
+      context.some_method(10).should == "foo context : 5"
 
       ctx do
         ctx.name.should == :anonymous
-        scoped.scoped_method(10).should == "anonymous context : 12"
+        context.some_method(10).should == "anonymous context : 12"
       end
     end
 
-    scoped.scoped_method(10).should == "original context : 10"
+    context.some_method(10).should == "original context : 10"
 
     class ::String
       ctx_define :+ do |other|
@@ -72,7 +73,6 @@ describe CTX do
     ctx {
       ("hello" + "world").should == "Hello World!"
     }
-
 
   end
 
